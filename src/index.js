be37acce24f88203e05a781b2fcf6ca24bc9d7a7
thomassr30 <thomassoto94@gmail.com>
2022-04-21 19:44:00 -0400

    Creación postModel y separacion express de index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import express from 'express'
-import postsRoutes from './routes/posts.routes.js'
 import { connectDB } from './db.js'
+import app from './app.js'
 
-const app = express()
+
 connectDB()
-app.use(postsRoutes)
+
 
 app.listen(4000)
 console.log('Servidor corriendo en puerto 4000')
