e69497e37bd5efd125078c458c739f043b80aa34
thomassr30 <thomassoto94@gmail.com>
2022-04-22 01:25:07 -0400

    uso de fileUpload

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import express from 'express'
+import fileUpload from 'express-fileupload'
 import postsRoutes from './routes/posts.routes.js'
 
 const app = express()
 app.use(express.json())
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: './upload'
+}))
+
 app.use(postsRoutes)
 
 
