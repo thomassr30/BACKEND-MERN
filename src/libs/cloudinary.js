8eb5182e59ba8362a12e582cdcb393b1508b1ac6
thomassr30 <thomassoto94@gmail.com>
2022-04-22 01:37:34 -0400

    Configuración cloudinary

---
+++ src/libs/cloudinary.js
@@ -0,0 +1,15 @@
+import {v2} from 'cloudinary'
+import  {config} from'dotenv'
+config()
+
+v2.config({
+    cloud_name:process.env.USER_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.SECRET_KEY
+})
+
+export const uploadImage = async (filePath) => {
+    return await v2.uploader.upload(filePath, {
+        folder: 'posts'
+    })
+}
