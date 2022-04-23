8cc9cd6e89744360276e5fbf979931d21e6940c7
thomassr30 <thomassoto94@gmail.com>
2022-04-23 19:16:20 -0400

    Solución error si el usuario no sube una imagen

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -29,7 +29,7 @@ export const createPost = async (req, res) => {
         const {title, description} = req.body
         let image
 
-        if(req.files.image){
+        if(req.files?.image){
             const result = await uploadImage(req.files.image.tempFilePath)
             
             image = {
