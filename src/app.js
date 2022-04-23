662ae91ed9ff627272687f243a4e660a683ae235
thomassr30 <thomassoto94@gmail.com>
2022-04-23 19:08:14 -0400

    Instalación e implementación de cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import fileUpload from 'express-fileupload'
 import postsRoutes from './routes/posts.routes.js'
+import cors from 'cors'
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 app.use(fileUpload({
     useTempFiles: true,
     tempFileDir: './upload'
